e3cb1c95551160f02a78cae11d6bf83b2f1dc6dc
блядун <gidjdfu26@mail.ru>
2025-09-29 22:18:30 +0500

    gfdfg

---
+++ егэ подготовка/правила/если/задания с если/05.py
@@ -0,0 +1,11 @@
+x = int(input())
+y = int(input())
+z = int(input())
+if (x % 2 == 0 or y % 2 == 0 or z % 2 == 0) and (x % 2 != 0 or y % 2 != 0 or z % 2 != 0):
+    print('da')
+else:
+    print('net')
+
+
+
+
---
+++ егэ подготовка/правила/если/задания с если/06.py
@@ -0,0 +1,7 @@
+s = input()
+if ('вор' in s) and ('ворон' not in s):
+    print('полиция')
+if  'ворон'  in s:
+    print('птица')
+if 'ворон' not in s and 'вор' not in s:
+    print('пусто')
