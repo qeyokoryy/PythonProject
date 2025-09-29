1f2e78cc0781c358b25eebd2407337d56cc8b451
блядун <gidjdfu26@mail.ru>
2025-09-29 21:08:41 +0500

    gfdfg

---
+++ егэ подготовка/правила/если/03.py
@@ -0,0 +1,23 @@
+#долгий путь
+x = 10
+y = 32
+z = 22
+
+if x > y and x > z:
+    print('x Больше')
+if y > x and y > z:
+    print('y Больше')
+if z > y and z > x:
+    print('z Больше')
+#быстрый
+x = 10
+y = 32
+z = 22
+
+if x > y and x > z:
+    print('x Больше')
+else:
+    if y>z:
+        print('y больше')
+    else:
+        print('z больше')
